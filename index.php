c09e2858dfa03b00ad92c26b97cdcb9e3fb52e50
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-01-29 17:03:11 -0800

    starting boogle project/styling site

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Boogle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+</head>
+<body>
+    <div class="wrapper index">
+        <div class="main">
+            <div class="logo">
+                <img src="assets/images/boogle.png" alt="">
+            </div>
+            <div class="search">
+                <form action="search.php" method="GET">
+                    <input class="searchInput" type="text" name="term">
+                    <input class="searchBtn" type="submit" value="Search">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
