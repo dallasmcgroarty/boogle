160bee2d54f635ff3a2f6defaaf59967d4001da1
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-01-29 19:16:11 -0800

    styled most of site

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    $_SESSION['prev_location'] = 'home'
+?>
 <!DOCTYPE html>
 <html>
 <head>
--- search.php
+++ search.php
@@ -1,3 +1,65 @@
 <?php
-    echo $_GET["term"];
+    session_start();
+    $check = $_SESSION['prev_location'];
+    
+    if(empty($_GET["term"]) && $check == 'home') {
+        header('Location: index.php');
+        exit();
+    }
+
+    if(!empty($_GET["term"])) {
+        $term = $_GET["term"];
+        $_SESSION['prev_location'] = 'search';
+    }
+
+    if(!empty($_GET["type"])) {
+        $type = $_GET["type"];
+    }
+    else {
+        $types = "sites";
+    }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Boogle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+                <div class="logo">
+                    <a href="index.php">
+                        <img src="assets/images/boogle.png" alt="">
+                    </a>
+                </div>
+                <div class="search">
+                    <form action="search.php" method="GET">
+                        <div class="searchBar">
+                            <input class="searchInput" type="text" name="term">
+                            <button class="searchBtn">
+                                <img src="assets/images/search_icon.png" alt="search icon from icons8.com https://icons8.com/icons/set/search">
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="tabs">
+                <ul class="tabList">
+                    <li class="<?php echo $type == 'sites' ? 'active': '' ?>"> 
+                        <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                            Sites
+                        </a>
+                    </li>
+                    <li class="<?php echo $type == 'images' ? 'active': '' ?>"> 
+                        <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+                            Images
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
